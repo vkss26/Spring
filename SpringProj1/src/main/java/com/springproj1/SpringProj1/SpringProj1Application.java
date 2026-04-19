7abefecf0bc0bcaa85683fc98cce5bfd1677a5a2
vedant <vedantsuryawanshi26@gmail.com>
2026-04-19 13:38:34 +0530

    initial setup proj
    Signed-off-by: vedant <vedantsuryawanshi26@gmail.com>

---
+++ SpringProj1/src/main/java/com/springproj1/SpringProj1/SpringProj1Application.java
@@ -0,0 +1,13 @@
+package com.springproj1.SpringProj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProj1Application.class, args);
+	}
+
+}
---
+++ SpringProj1/src/test/java/com/springproj1/SpringProj1/SpringProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproj1.SpringProj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
